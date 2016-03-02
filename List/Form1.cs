8b9d8694d2206829b0d8b9b63ea6b329997af514
mehmetgelmedi <mehmetemingelmedi@gmail.com>
2016-03-02 23:17:35 +0200

    Dosyalar Yuklendi.

---
+++ List/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            LinkedList ll = new LinkedList();
+            ll.InsertFirst(4);
+            //ll.InsertLast(5);
+            MessageBox.Show(ll.DisplayElements());
+            ll.InsertFirst(1);
+            ll.InsertFirst(2);
+            ll.InsertFirst(8);
+            MessageBox.Show(ll.DisplayElements());
+            ll.DeleteFirst();
+            MessageBox.Show(ll.DisplayElements());
+            //ll.DeleteLast();
+            MessageBox.Show(ll.DisplayElements());
+            //ll.DeleteLast();
+            //ll.DeleteLast();
+            MessageBox.Show(ll.DisplayElements());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LinkedList ll = new LinkedList();
+            ll.InsertFirst(6); 
+            ll.InsertFirst(5); 
+            ll.InsertFirst(4);
+            ll.InsertFirst(3);
+            ll.InsertFirst(2);
+            ll.InsertFirst(1);
+
+            Uygulama2(ll.Head);
+        }
+
+        public void Uygulama2(Node test)
+        {
+            if (test == null)
+                return;
+
+            MessageBox.Show(test.Data.ToString());
+
+            if (test.Next != null)
+                Uygulama2(test.Next.Next);
+
+            MessageBox.Show(test.Data.ToString());
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            LinkedList ll = new LinkedList();
+            ll.InsertFirst(6);
+            ll.InsertFirst(5);
+            ll.InsertFirst(4);
+            ll.InsertFirst(3);
+            ll.InsertFirst(2);
+            ll.InsertFirst(1);
+            MessageBox.Show(ll.DisplayElements());
+            Uygulama3Reverse(ref ll.Head);
+            MessageBox.Show(ll.DisplayElements());
+        }
+
+        public void Uygulama3Reverse(ref Node head)
+        {
+		    Node currNode = head;
+		    Node nextNode = null;
+		    Node prevNode = null;
+
+		    while(currNode.Next!=null)
+            {
+			    nextNode = currNode.Next;
+			    currNode.Next = prevNode;
+			    prevNode = currNode;
+			    currNode = nextNode;
+		    }
+            currNode.Next = prevNode;
+            head = currNode;
+        }
+
+    }
+}
---
+++ List/LinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class LinkedList : LinkedListADT
+    {
+        public override void InsertFirst(int value)
+        {
+            Node tmpHead = new Node 
+            { 
+                Data = value 
+            };
+
+            if (Head == null)
+                Head = tmpHead;
+            else
+            {
+                //En kritik nokta: tmpHead'in next'i eski Head'i göstermeli
+                tmpHead.Next = Head;
+                //Yeni Head artık tmpHead oldu
+                Head = tmpHead;
+            }
+            //Bağlı listedeki eleman sayısı bir arttı
+            Size++;
+        }
+
+        public override void InsertLast(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertPos(int position, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteFirst()
+        {
+            if (Head != null)
+            {
+                //Head'in next'i HeadNext'e atanıyor
+                Node HeadNext = this.Head.Next;
+                //HeadNext null ise zaten tek kayıt olan Head silinir.
+                if (HeadNext == null)
+                    Head = null;
+                else
+                    //HeadNext null değilse yeni Head, HeadNext olur.
+                    Head = HeadNext;
+                //Listedeki eleman sayısı bir azaltılıyor
+                Size--;
+            }
+        }
+
+        public override void DeleteLast()
+        {
+            //burayı yazacaz..
+        }
+
+        public override void DeletePos(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Node GetElement(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string DisplayElements()
+        {
+            string temp = "";
+            Node item = Head;
+            while (item != null)
+            {
+                temp += "-->" + item.Data;
+                item = item.Next;
+            }
+
+            return temp;
+        }
+    }
+}
